51c21fd78852a93f2a46e71909744e93fc86a8bc
Argian Ardi Prasetya <argianardi1435@gmail.com>
2022-11-28 11:02:15 +0700

    add Start Quiz page

---
+++ pages/start-quiz/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import withProtected from "../../hoc/withProtected";
+
+const StartQuiz = () => {
+  return (
+    <div>
+      <p>StartQuiz</p>
+    </div>
+  );
+};
+
+export default withProtected(StartQuiz);
